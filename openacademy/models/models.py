8fa84ebd48212060b9179eeeb7e1ac3036da17db
fah <fah@odoo.com>
2019-01-10 13:06:53 +0100

    Added Course model

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -6,3 +6,9 @@ class openacademy(models.Model):
     _name = 'openacademy.openacademy'
 
     name = fields.Char()
+
+class Course(models.Model):
+    _name = 'openacademy.course'
+
+    name = fields.Char(string="Title", required=True)
+    description = fields.Text()
