11316fb6b41ffe845205126462e82248a13e8e3b
fah <fah@odoo.com>
2019-01-11 08:26:35 +0100

    Correction comment in models.py
    - Many2one(one_thing,...): Many "things" to "one_thing"
    - One2many(...,one_thing)

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -29,7 +29,7 @@ class Session(models.Model):
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
 
-    # There can be several teachers for a course session
+    # One instructor can teach several course sessions
     instructor_id = fields.Many2one('res.partner', string="Instructor")
 
     # There can be several sessions of a course
