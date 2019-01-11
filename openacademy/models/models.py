01f36f31e5fd8180394c5d82fe32f380f6bc31f9
fah <fah@odoo.com>
2019-01-11 09:14:28 +0100

    Added parameters in models
    - state: state of a session
    - archive: is a session archived or not
    - level: level of a course

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -20,6 +20,8 @@ class Course(models.Model):
     # course_id corresponds to session_ids                                 
     session_ids=fields.One2many('openacademy.session', 'course_id', string="Sessions")
 
+    level = fields.Selection(
+        [(1, 'Easy'), (2, 'Medium'), (3, 'Hard')], string="Difficulty Level")
 
 class Session(models.Model):
     _name = 'openacademy.session'
@@ -29,9 +31,11 @@ class Session(models.Model):
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
 
+    archive = fields.Boolean(default=False)
     # One instructor can teach several course sessions
     instructor_id = fields.Many2one('res.partner', string="Instructor")
-
+    state = fields.Selection([('draft', "Draft"), ('confirmed', "Confirmed"), ('done', "Done")], default='draft')
+    
     # There can be several sessions of a course
     course_id = fields.Many2one('openacademy.course',
                                 ondelete='cascade', string="Course", required=True)
