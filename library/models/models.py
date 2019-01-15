8048e4f47b90edeadd6c4d8f89dbcc6e8f96087f
fah <fah@odoo.com>
2019-01-15 12:58:16 +0100

    [FIX] library: remove useless files [ADD] openacademy: calendar (doesn't work)

--- library/models/models.py
+++ library/models/models.py
@@ -19,18 +19,6 @@ class Book(models.Model):
 
     rental_ids = fields.One2many('library.rental', 'book_id', string='Rentals')
 
-class BookInstance(models.Model):    
-     _name = 'library.bookinstance'
-     
-#     _description = 'Book instance'
-
-#     customer_id = fields.Many2one('library.customer', string='Customer')
-#     book_id = fields.Many2one('library.book', string='Book')
-
-#     rental_date = fields.Date()
-#     return_date = fields.Date()
-
-
 class Customer(models.Model):
     _name = 'library.customer'
     _description = 'Customer'
--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -2,7 +2,7 @@
 
 # exceptions is for constrains()
 from odoo import api, exceptions, fields, models
-
+from datetime import timedelta
 
 class openacademy(models.Model):
     _name = 'openacademy.openacademy'
@@ -71,10 +71,36 @@ class Session(models.Model):
 
     name = fields.Char(required=True)
 
-    # The start date is today if not specified
     start_date = fields.Date(default=fields.Date.today)
+    end_date = fields.Date(string="End Date", store=True,
+                           compute='_get_end_date', inverse='_set_end_date')
+
     duration = fields.Float(digits=(6, 2), help="Duration in days")
-    end_date = fields.Date(default=fields.Date.today)
+
+    @api.depends('start_date', 'duration')
+    def _get_end_date(self):
+        for r in self:
+            if not (r.start_date and r.duration):
+                r.end_date = r.start_date
+                continue
+
+            # Add duration to start_date, but: Monday + 5 days = Saturday, so
+            # subtract one second to get on Friday instead
+            start = fields.Datetime.from_string(r.start_date)
+            duration = timedelta(days=r.duration, seconds=-1)
+            r.end_date = start + duration
+
+    def _set_end_date(self):
+        for r in self:
+            if not (r.start_date and r.end_date):
+                continue
+
+            # Compute the difference between dates, but: Friday - Monday = 4 days,
+            # so add one day to get 5 days instead
+            start_date = fields.Datetime.from_string(r.start_date)
+            end_date = fields.Datetime.from_string(r.end_date)
+            r.duration = (end_date - start_date).days + 1
+
 
     seats = fields.Integer(string="Number of seats")
 
