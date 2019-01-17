d596dff6afb97ec3233c0de2377564e38ff28a75
fah <fah@odoo.com>
2019-01-17 08:39:15 +0100

    [ADD] openacademy: Controller

--- openacademy/__manifest__.py
+++ openacademy/__manifest__.py
@@ -23,21 +23,23 @@
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category':    'OpenAcademy',
-    'version':     '0.8',
+    'version':     '0.9',
 
     # any module necessary for this one to work correctly
-    'depends':     ['base', 'mail', 'account', 'product', 'board'],
-
+    # check, maybe mail is included in website 
+    # 'depends':     ['base', 'mail', 'account', 'product', 'board', 'website', 'website_sale'],
+    'depends': ['base', 'board', 'website', 'website_sale'],
     # always loaded
     'data':        [
         "security/security.xml",
         "security/ir.model.access.csv",
-        "demo/demo.xml",
+        "data/data.xml",
         "views/course_view.xml",
         "views/partner_view.xml",
         "views/session_view.xml",
         "views/menu_view.xml",
         "report/report.xml",
+        "templates/templates.xml",
     ],
     # only loaded in demonstration mode
     'demo':        [
